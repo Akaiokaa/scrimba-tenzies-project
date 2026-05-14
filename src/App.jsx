e82a34159036bfb2829682e6478583f127b67da2
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-05-14 15:23:29 -0700

    css for dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
-
+import Die from './components/Die.jsx'
 function App() {
-
   return (
     <>
-    <main>
-      type something here
+    <main className='die-container'>
+      <div className='box'>
+        <div className='first-row'>
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+        </div>
+        <div className='second-row'>
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+        </div>
+      </div>
+      
     </main>
     </>
   )
---
+++ src/components/Die.jsx
@@ -0,0 +1,8 @@
+import './Die.css'
+
+function Die(props) {
+    return(
+        <button className='die'>{props.value}</button>
+    )
+}
+export default Die;
