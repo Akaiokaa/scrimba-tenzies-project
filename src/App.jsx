80c8c949bb8370126bde9fb54a0c7a2bf2b45d7b
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-05-14 15:30:43 -0700

    added generate dice function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import './App.css'
 import Die from './components/Die.jsx'
 function App() {
+  function generateAllNewDice(){
+    const newDice = []
+    for(let i = 0; i < 10; i++){
+      const rand = Math.ceil(Math.random() * 6)
+      newDice.push(rand)
+    }
+    return newDice
+  }
+  console.log(generateAllNewDice())
   return (
     <>
     <main className='die-container'>
